67bfda832de46fd239cb204400e6e27ee1f335d6
cobyd <36939461+cobyd@users.noreply.github.com>
2018-03-05 14:57:26 -0500

    reworking contract to be scalable

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,59 +1,61 @@
 pragma solidity ^0.4.19;
 
-contract ThreeWaySplitter {
-    // given a small number of users, arrays seem like a reasonable solution
-    // mapping has the issue of checking the balance, but also whether or not they are a valid user
-    // mapping(address => User) seems like a good approach for more scaling
-    address[3] public users;
-    uint[3] public userBalances;
+contract Splitter {
+    mapping(address=>uint) users;
+    address owner;
     
-    // requirement - there are 3 people: Alice, Bob and Carol
-    function ThreeWaySplitter(address[3] _users) public {
-        users = _users;
-    }
+    /*
     
-    // requirement - whenever Alice sends ether to the contract, half of it goes to Bob and the other half to Carol
-    function() public payable {
-        bool validUser;
-        uint8 idx;
-        (validUser, idx) = validateUserAndGetIndex(msg.sender);
-        require(validUser);
-        split(msg.sender, msg.value);
+    Splitter contract:
+    
+    ether in -> only payable function is splitFromTransaction.  When a 
+                user calls this function, the value of the transaction
+                will be split between their given target addresses
+    
+    ether out -> A user can call withdraw to cash out their balance
+                 The owner can call killswitch to cash out all users (not yet implemented)
+    
+    other state changes -> splitFromBalance allows a user to split ether that is 
+                           in their balance without sending any additional ether
+    
+    */
+    
+    // requirement - make the contract a utility that can be used by David, Emma and 
+    // anybody with an address to split Ether between any 2 other addresses of their own choice
+    function Splitter() public {
+        owner = msg.sender;
     }
     
     // requirement - Alice can use the Web page to split her ether
-    function splitFromBalance(uint amount) public {
-        bool validUser;
-        uint8 idx;
-        (validUser, idx) = validateUserAndGetIndex(msg.sender);
-        require(validUser);
-        uint userBalance = userBalances[idx];
-        require(userBalance >= amount);
-        setUserBalance(msg.sender, userBalance - amount);
-        split(msg.sender, amount);
+    function splitFromBalance(uint valueToSplit, address[2] to) public {
+        require(msg.sender != to[0]);
+        require(msg.sender != to[1]);
+        uint userBalance = users[msg.sender];
+        require(userBalance >= valueToSplit);
+        users[msg.sender] -= valueToSplit;
+        split(to, valueToSplit);
     }
     
-    function split(address from, uint amount) private {
-        uint8[2] memory otherUsers;
-        otherUsers = getOtherUserIndexes(from);
-        uint newValue;
-        if(isOdd(amount))
-            newValue = (amount -1) / 2; // I'll be taking that...
-        else
-            newValue = amount / 2;
-        for(uint8 i = 0; i <= 1; i++) {
-            uint oldValue = userBalances[otherUsers[i]];
-            userBalances[otherUsers[i]] = oldValue + newValue;
-        }
+    // requirement - whenever Alice sends ether to the contract, half of it goes to Bob and the other half to Carol
+    function splitFromTransaction(address[2] to) public payable {
+        require(msg.sender != to[0]);
+        require(msg.sender != to[1]);
+        uint valueToSplit = msg.value;
+        split(to, valueToSplit);
     }
     
-    // Nice to have state readers
-    function getUsers() public view returns(address[3]) {
-        return users;
+    // private function that is used by splitFromBalance and splitFromTransaction
+    function split(address[2] to, uint valueToSplit) private {
+        require(to[0] != to[1]);
+        uint half = valueToSplit / 2;
+        users[to[0]] += half;
+        users[to[1]] += half;
+        // extra 1 wei in some cases will be credited to contract owner upon killswitch
     }
     
-    function getUserBalances() public view returns(uint[3]) {
-        return userBalances;
+    function withdraw() public {
+        msg.sender.transfer(users[msg.sender]);
+        users[msg.sender] = 0;
     }
     
     // requirement - we can see the balance of the Splitter contract on the Web page
@@ -63,43 +65,6 @@ contract ThreeWaySplitter {
     
     // requirement - we can see the balances of Alice, Bob and Carol on the Web page
     function getUserBalance(address user) public view returns(uint) {
-        bool validUser;
-        uint8 idx;
-        (validUser, idx) = validateUserAndGetIndex(user);
-        require(validUser);
-        return userBalances[idx];
-    }
-    
-    function setUserBalance(address user, uint newValue) private {
-        bool validUser;
-        uint8 idx;
-        (validUser, idx) = validateUserAndGetIndex(user);
-        userBalances[idx] = newValue;
-    }
-    
-    // Other helper nonsense
-    function validateUserAndGetIndex(address user) private view returns(bool, uint8) {
-        for(uint8 i = 0; i <= 2; i++) {
-            if(users[i] == user) {
-                return (true, i);
-            }
-        }
-        return (false, 0);
-    }
-    
-    function getOtherUserIndexes(address user) private view returns(uint8[2]) {
-        uint8 userCount = 0;
-        uint8[2] memory otherUsers;
-        for(uint8 i = 0; i <= 2; i++) {
-            if(users[i] != user) {
-                otherUsers[userCount] = i;
-                userCount++;
-            }
-        }
-        return otherUsers;
-    }
-    
-    function isOdd(uint amount) private pure returns(bool) {
-        return amount % 2 == 1;
+        return users[user];
     }
 }
