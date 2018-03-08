3228cbf25ee984407ded306b968f9356bb1cec75
Coby VBox <coby5252@gmail.com>
2018-03-07 23:26:37 -0500

    had to change solidity signatures bcause truffle didn't like arrays?  FIxed test files up a little bit, but still not quite there

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -26,18 +26,18 @@ contract Splitter is Stoppable {
         active = true;
     }
 
-    event LogSplit(address indexed from, address[2] indexed to, uint value);
+    event LogSplit(address indexed from, address indexed toOne, address indexed toTwo, uint value);
     event LogWithdrawl(address indexed to, uint value, bool forced);
     
-    function split(address[2] to) public payable isActive {
-        require(msg.sender != to[0]);
-        require(msg.sender != to[1]);
-        require(to[0] != to[1]);
-        LogSplit(msg.sender,to,msg.value);
+    function split(address toOne, address toTwo) public payable isActive {
+        require(msg.sender != toOne);
+        require(msg.sender != toTwo);
+        require(toOne != toTwo);
+        LogSplit(msg.sender, toOne, toTwo, msg.value);
         uint valueToSplit = msg.value;
         uint half = valueToSplit / 2;
-        balances[to[0]] += half;
-        balances[to[1]] += half;
+        balances[toOne] += half;
+        balances[toTwo] += half;
         balances[owner] += valueToSplit % 2;
     }
     
@@ -49,7 +49,7 @@ contract Splitter is Stoppable {
         msg.sender.transfer(toSend);
     }
     
-    function validateInput(address[2] to) private view {
+    function validateInput(address[] to) private view {
         require(msg.sender != to[0]);
         require(msg.sender != to[1]);
         require(to[0] != to[1]);
--- test/splitter.js
+++ test/splitter.js
@@ -1,112 +1,63 @@
 describe("Splitter", () => {
   const Splitter = artifacts.require("./Splitter.sol");
+  let account_0;
+  let account_1;
+  let account_2;
+  let account_3;
+  // let splitter;
   beforeEach(() => {
-    web3.eth.getAccounts((err, _accounts) => {
-      return Splitter.new({ from: accounts[0], gas: 3000000 }).then(
-        _splitter => {
-          const splitter = _splitter;
-        }
-      );
-    });
+    new Promise((resolve, reject) => {
+      web3.eth.getAccounts((err, _accounts) => {
+        console.log("accounts");
+        if (err) reject(err);
+        else resolve(_accounts);
+      });
+    })
+      .then(_accounts => {
+        console.log("setting accounts");
+        account_0 = _accounts[0];
+        account_1 = _accounts[1];
+        account_2 = _accounts[2];
+        account_3 = _accounts[3];
+      })
+      .then(() => {
+        console.log("new splitter");
+        console.log(account_0);
+        Splitter.new({ from: account_0 });
+      })
+      .then(_splitter => {
+        console.log("setting splitter");
+        console.log(_splitter);
+        // splitter = _splitter;
+      });
   });
   contract("Splitter", () => {
     it("should split ether", () => {
-      web3.eth.getAccounts((err, _accounts) => {
-        const account_0 = _accounts[0];
-        const account_1 = _accounts[1];
-        const account_2 = _accounts[2];
-        const account_3 = _accounts[3];
-        return Splitter.new({ from: account_0 }).then(_splitter => {
-          const splitter = _splitter;
-          splitter
-            .split([account_2, account_3], {
-              from: account_1,
-              value: web3.toWei(2, "ether"),
-              gas: 300000
-            })
-            .then(() => {
-              // splitter.balances(account_1).then(function(_balance) {
-              //   conslole.log("here!");
-              //   assertEqual(_balance, web3.toWei(1, "ether"));
-              // });
+      return Splitter.new({ from: account_0 }).then(_splitter => {
+        const splitter = _splitter;
+        splitter
+          .split(account_2, account_3, {
+            from: account_1,
+            value: web3.toWei(2, "finney"),
+            gas: 300000
+          })
+          .then(() => {
+            splitter.balances(account_2).then(function(_balance) {
+              console.log(
+                _balance.toString(),
+                " should be ",
+                web3.toWei(1, "finney")
+              );
+              assert.equal(_balance.toNumber(), web3.toWei(1, "finney"));
             });
-        });
+          });
+      });
+    });
+    it("should withdraw ether", () => {
+      console.log(account_2);
+      return web3.eth.getBalance(account_2, _balance => {
+        console.log(_balance);
       });
     });
   });
 });
-
-// it("should call a function that depends on a linked library", function() {
-//   var meta;
-//   var metaCoinBalance;
-//   var metaCoinEthBalance;
-
-//   return MetaCoin.deployed()
-//     .then(function(instance) {
-//       meta = instance;
-//       return meta.getBalance.call(accounts[0]);
-//     })
-//     .then(function(outCoinBalance) {
-//       metaCoinBalance = outCoinBalance.toNumber();
-//       return meta.getBalanceInEth.call(accounts[0]);
-//     })
-//     .then(function(outCoinBalanceEth) {
-//       metaCoinEthBalance = outCoinBalanceEth.toNumber();
-//     })
-//     .then(function() {
-//       assert.equal(
-//         metaCoinEthBalance,
-//         2 * metaCoinBalance,
-//         "Library function returned unexpected function, linkage may be broken"
-//       );
-//     });
-// });
-// it("should send coin correctly", function() {
-//   var meta;
-
-//   // Get initial balances of first and second account.
-//   var account_one = accounts[0];
-//   var account_two = accounts[1];
-
-//   var account_one_starting_balance;
-//   var account_two_starting_balance;
-//   var account_one_ending_balance;
-//   var account_two_ending_balance;
-
-//   var amount = 10;
-
-//   return MetaCoin.deployed()
-//     .then(function(instance) {
-//       meta = instance;
-//       return meta.getBalance.call(account_one);
-//     })
-//     .then(function(balance) {
-//       account_one_starting_balance = balance.toNumber();
-//       return meta.getBalance.call(account_two);
-//     })
-//     .then(function(balance) {
-//       account_two_starting_balance = balance.toNumber();
-//       return meta.sendCoin(account_two, amount, { from: account_one });
-//     })
-//     .then(function() {
-//       return meta.getBalance.call(account_one);
-//     })
-//     .then(function(balance) {
-//       account_one_ending_balance = balance.toNumber();
-//       return meta.getBalance.call(account_two);
-//     })
-//     .then(function(balance) {
-//       account_two_ending_balance = balance.toNumber();
-
-//       assert.equal(
-//         account_one_ending_balance,
-//         account_one_starting_balance - amount,
-//         "Amount wasn't correctly taken from the sender"
-//       );
-//       assert.equal(
-//         account_two_ending_balance,
-//         account_two_starting_balance + amount,
-//         "Amount wasn't correctly sent to the receiver"
-//       );
-//     });
-// });
